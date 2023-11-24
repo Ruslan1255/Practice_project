45cd6e5e2843ebddbfcd9825d54aa62639ed98d6
Ruslan Mirzeliyev <ruslanmirzliyev367@gmail.com>
2023-11-24 11:08:56 +0400

    JS DOM TASK

---
+++ JAVASCRIPT/JS DOM TASK/main.js
@@ -0,0 +1,44 @@
+"use strict";
+// TASK 1
+// let btn = document.querySelector(".btn");
+// let body = document.querySelector("body");
+// let colors = ["#0C356A", "#0174BE", "#FFC436", "#FFF0CE"];
+
+// btn.addEventListener("click", () => {
+//   let i = Math.floor(Math.random() * 4);
+//   if (i != colors.length) {
+//     body.style.backgroundColor = colors[i];
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+// });
+
+// TASK 2
+// let btn = document.querySelector(".counter");
+// let display = document.querySelector(".text");
+// let i = 0;
+// display.textContent = "0";
+// btn.addEventListener("click", () => {
+//   i++;
+//   display.textContent = i;
+// });
+
+// TASK 3
+
+// let images = [
+//   "headset.png",
+//   "girl_1.avif",
+//   "girl_2.avif",
+//   "guy_1.avif",
+//   "guy_2.avif",
+//   "guy_3.avif",
+// ];
+// let image = document.querySelector(".img");
+// let srcChanger = document.querySelector(".srcChanger");
+
+// srcChanger.addEventListener('click',()=>{
+//   let i = Math.floor(Math.random() * images.length)
+//   image.setAttribute('src',images[i]);
+// })
+
