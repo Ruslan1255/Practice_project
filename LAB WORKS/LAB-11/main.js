a893834bea50e0c7fcae883438e93ebebc313d11
Ruslan Mirzeliyev <ruslanmirzliyev367@gmail.com>
2023-12-03 22:20:17 +0400

    LAB-11

---
+++ LAB WORKS/LAB-11/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const inputTask = document.getElementById("inputTask");
+const taskList = document.getElementById("taskList");
+const addBtn = document.querySelector(".add_btn");
+
+addBtn.addEventListener("click", () => {
+  if (inputTask.value.trim() === "") {
+    alert("Please fill the input");
+    return;
+  }
+
+  const taskItem = document.createElement("li");
+  taskItem.textContent = inputTask.value;
+
+  const removeBtn = document.createElement("i");
+  removeBtn.classList.add("fa-solid");
+  removeBtn.classList.add("fa-times");
+
+  taskItem.appendChild(removeBtn);
+
+  const listBox = document.createElement("div");
+  listBox.appendChild(taskItem);
+  listBox.appendChild(removeBtn);
+
+  taskList.appendChild(listBox);
+
+  removeBtn.addEventListener("click", () => {
+    listBox.remove();
+  });
+
+  inputTask.value = "";
+});
