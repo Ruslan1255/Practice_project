4a85980303149ee3ba09d50f007c0b5fe2f0d1ea
Ruslan <ruslanmirzliyev367@gmail.com>
2023-12-26 22:06:18 +0400

    JS TASK 6 SLIDER

--- JAVASCRIPT/JS TASK-6/main.js
+++ JAVASCRIPT/JS TASK-6/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+let img_box = document.querySelector(".slide_img");
+let left = document.querySelector(".icon_left");
+let right = document.querySelector(".icon_right");
+
+const arr = [
+  "photos/Screenshot (12).png",
+  "photos/Screenshot (13).png",
+  "photos/Screenshot (14).png",
+  "photos/Screenshot (15).png",
+];
+
+let i = 0;
+
+left.addEventListener("click", () => {
+  i = (i - 1 + arr.length) % arr.length;
+  img_box.setAttribute("src", arr[i]);
+});
+
+right.addEventListener("click", () => {
+  i = (i + 1) % arr.length;
+  img_box.setAttribute("src", arr[i]);
+});
