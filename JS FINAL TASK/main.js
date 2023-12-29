90771243a016da65cda4159db902ec5da32969a3
Ruslan <ruslanmirzliyev367@gmail.com>
2023-12-30 01:36:16 +0400

    JS FINAL TASK

--- JS FINAL TASK/main.js
+++ JS FINAL TASK/main.js
@@ -48,13 +48,17 @@ function initApp(data) {
 
 function addToCart(key) {
   if (cartItems[key] == null) {
-      cartItems[key] = { ...data.products[key], quantity: 1, originalPrice: data.products[key].price };
+    cartItems[key] = {
+      ...data.products[key],
+      quantity: 1,
+      originalPrice: data.products[key].price,
+    };
   } else {
-      cartItems[key].quantity++;
+    cartItems[key].quantity++;
   }
   updateCart();
+  setCartItemsToLocalStorage(cartItems);
 }
-
 
 function updateCart() {
   cartItemList.innerHTML = "";
@@ -74,9 +78,13 @@ function updateCart() {
                   data.products[key].price * cartItem.quantity
                 ).toLocaleString()}</div>
                 <div>
-                    <button onclick="changeQuantity(${key},${cartItem.quantity - 1})">-</button>
+                    <button onclick="changeQuantity(${key},${
+        cartItem.quantity - 1
+      })">-</button>
                     <div class="count">${cartItem.quantity}</div>
-                    <button onclick="changeQuantity(${key},${cartItem.quantity + 1})" class="plusBTN">+</button>
+                    <button onclick="changeQuantity(${key},${
+        cartItem.quantity + 1
+      })" class="plusBTN">+</button>
                     <div class="trash-icon"><i class="fa-solid fa-trash"></i></div>
                 </div>
             `;
@@ -95,16 +103,26 @@ function updateCart() {
 function deleteCartItem(key) {
   delete cartItems[key];
   updateCart();
+  setCartItemsToLocalStorage(cartItems);
 }
 
 function changeQuantity(key, quantity) {
   if (quantity == 0) {
-      delete cartItems[key];
+    delete cartItems[key];
   } else {
-      cartItems[key].quantity = quantity;
-      cartItems[key].price = quantity * cartItems[key].originalPrice;
+    cartItems[key].quantity = quantity;
+    cartItems[key].price = quantity * cartItems[key].originalPrice;
   }
   updateCart();
+  setCartItemsToLocalStorage(cartItems);
 }
 
+function getCartItemsFromLocalStorage() {
+  const cartItemsJSON = localStorage.getItem("cartItems");
+  return cartItemsJSON ? JSON.parse(cartItemsJSON) : [];
+}
 
+function setCartItemsToLocalStorage(cartItems) {
+  const cartItemsJSON = JSON.stringify(cartItems);
+  localStorage.setItem("cartItems", cartItemsJSON);
+}
