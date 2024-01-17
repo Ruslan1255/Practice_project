a22f7d5b6b9fcc0a3934cfee5b0cd1ebac625797
Ruslan <ruslanmirzliyev367@gmail.com>
2024-01-17 19:22:08 +0400

    LAB-15 REACT

---
+++ LAB WORKS/lab-15/src/data/data.jsx
@@ -0,0 +1,27 @@
+export const Cars = [
+  {
+    title: "BMW",
+    price: 25000,
+    id: 1,
+  },
+  {
+    title: "Mercedes-Benz",
+    price: 40000,
+    id: 2,
+  },
+  {
+    title: "Hundai",
+    price: 20000,
+    id: 3,
+  },
+  {
+    title: "Audi",
+    price: 30000,
+    id: 4,
+  },
+  {
+    title: "Kia",
+    price: 50000,
+    id: 5,
+  },
+]
--- LAB WORKS/lab-15/src/layout/Main/main.jsx
+++ LAB WORKS/lab-15/src/layout/Main/main.jsx
@@ -1,36 +1,9 @@
 import React from "react";
+import { Cars } from "../../data/data";
 import Card from "../../components/card/card.jsx";
 import "./main.css";
 
 const main = () => {
-  const Cars = [
-    {
-      title: "BMW",
-      price: 25000,
-      id: 1,
-    },
-    {
-      title: "Mercedes-Benz",
-      price: 40000,
-      id: 2,
-    },
-    {
-      title: "Hundai",
-      price: 20000,
-      id: 3,
-    },
-    {
-      title: "Audi",
-      price: 30000,
-      id: 4,
-    },
-    {
-      title: "Kia",
-      price: 50000,
-      id: 5,
-    },
-  ];
-
   return (
     <div className="main_box">
       {Cars.map((props) => (
