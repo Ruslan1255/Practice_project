c9609ddc3788f7f3f04abd83d55fcc24669f71ae
Ruslan <ruslanmirzliyev367@gmail.com>
2024-01-21 12:39:26 +0400

    LAB-16

---
+++ LAB_WORKS/lab-16/src/Data/Slider_data.jsx
@@ -0,0 +1,5 @@
+import code from "../assets/images/code.avif";
+import code2 from "../assets/images/code2.jpg";
+import code3 from "../assets/images/code3.avif";
+
+export const images = [code, code2, code3];
