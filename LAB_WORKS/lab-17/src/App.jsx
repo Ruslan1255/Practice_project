902b1bb3a9785fa9f4ff94a886aa950c60cb1239
Ruslan <ruslanmirzliyev367@gmail.com>
2024-01-28 14:16:59 +0400

    LAB-17

---
+++ LAB_WORKS/lab-17/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./App.css";
+import Card from "../src/Components/Card/Card.jsx";
+
+const App = () => {
+  const animalsData = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  function getRandomAnimal() {
+    return animalsData[Math.floor(Math.random() * animalsData.length)];
+  }
+
+  const [animals, setAnimals] = useState([]);
+
+  function clickRandom() {
+    setAnimals([...animals, getRandomAnimal()]);
+  }
+
+  return (
+    <>
+      <button className="btn" onClick={clickRandom}>
+        Get random animal
+      </button>
+      <div className="app">
+        {animals.map((title, index) => {
+          return <Card title={title} key={index} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ LAB_WORKS/lab-17/src/Components/Data/animalCard.jsx
@@ -0,0 +1,15 @@
+import bird from "../../assets/images/bird.svg";
+import cat from "../../assets/images/cat.svg";
+import cow from "../../assets/images/cow.svg";
+import dog from "../../assets/images/dog.svg";
+import gator from "../../assets/images/gator.svg";
+import horse from "../../assets/images/horse.svg";
+
+export const animalsImg = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
