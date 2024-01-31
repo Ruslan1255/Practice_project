1194b9d06ac249d958ff1571ed36caf55fb88c89
Ruslan <ruslanmirzliyev367@gmail.com>
2024-01-31 23:57:48 +0400

    REACT TASK 3

---
+++ REACT/react_task_3/src/Layout/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from './header.module.css'
+import { CiShoppingCart } from "react-icons/ci";
+import { RxAvatar } from "react-icons/rx";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div className={styles.header_left}>
+          <h1>Sneakers</h1>
+          <ul>
+            <li>
+              <a href="">Collections</a>
+            </li>
+            <li>
+              <a href="">Men</a>
+            </li>
+            <li>
+              <a href="">Women</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.header_right}>
+          <CiShoppingCart className={styles.header_icon} />
+          <RxAvatar className={styles.header_icon} />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
